0304c5a055130979bc6cdc4839734501b0e35c57
Kaarlo Sasiang <sasiang64@gmail.com>
2024-06-26 05:00:45 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { TypographyH1 } from "./components/typography";
+
+function App() {
+  return (
+    <>
+      <TypographyH1>Hello World</TypographyH1>
+    </>
+  );
+}
+
+export default App;
