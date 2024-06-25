e56ebe5acd0a2e890f320ccc3bd07dd37956a0b1
Kaarlo Sasiang <sasiang64@gmail.com>
2024-06-26 05:27:05 +0800

    initialized acernity-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { TypographyH1 } from "./components/typography";
+import { FlipWords } from "./components/acernity-ui/FlipWords";
 
 function App() {
+  const words = ["handsome", "cute", "kind", "awesome!"];
   return (
     <>
-      <TypographyH1>Hello World</TypographyH1>
+      <div className="h-screen flex justify-center items-center px-4">
+        <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+          Kaarlo Sasiang is
+          <FlipWords
+            words={words}
+            className={"transition-all duration-100 ease-in-out"}
+          />
+        </div>
+      </div>
     </>
   );
 }
