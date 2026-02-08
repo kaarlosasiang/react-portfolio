59b687cf2a7f7ec1a40c7fc1363f87887ed705ee
Kaarlo Sasiang <sasiang64@gmail.com>
2026-02-08 16:27:28 +0800

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kaarlosasiang.com',
+  site: 'https://kaarlosasiang.vercel.app',
   output: 'static',
   integrations: [
     react(),
